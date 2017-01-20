83f4a2949338e6f0e79bf48eb99bc76b1e94daef
Beakman <migueldelcastillo@live.com>
2017-01-20 14:03:42 -0600

    Explorer app modified with Springfield WMS

--- springfield.js
+++ springfield.js
@@ -8,7 +8,8 @@ function eventWindowLoaded() {
 
     // Add some image layers to the World Window's globe.
     wwd.addLayer(new WorldWind.BMNGOneImageLayer());
-    wwd.addLayer(new WorldWind.BingAerialWithLabelsLayer());
+    //wwd.addLayer(new WorldWind.BingAerialWithLabelsLayer());
+    wwd.addLayer(new WorldWind.OpenStreetMapImageLayer());
 
     // Add a compass, a coordinates display and some view controls to the World Window.
     wwd.addLayer(new WorldWind.CompassLayer());
